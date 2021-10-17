3326443c0843f1c74a8bf786cf66a1023e96de78
Jovakinn <max05012004@gmail.com>
2021-10-17 23:17:54 +0300

    add thread status check

--- src/main/java/org/multithreading/service/model/Worker.java
+++ src/main/java/org/multithreading/service/model/Worker.java
@@ -1,6 +1,10 @@
 package org.multithreading.service.model;
 
+import java.util.logging.Logger;
+
 public class Worker implements Runnable {
+
+    private final static Logger LOG = Logger.getLogger(Worker.class.getName());
 
     @Override
     public void run() {
@@ -16,8 +20,14 @@ public class Worker implements Runnable {
     public static void main(String[] args) throws InterruptedException {
         System.out.println("Method main begins");
         Thread thread = new Thread(new Worker());
+
+        LOG.info(String.valueOf(thread.getState()));
         thread.start();
+
+        LOG.info(String.valueOf(thread.getState()));
         thread.join(1500);
+
+        LOG.info(String.valueOf(thread.getState()));
         System.out.println("Main thread ended");
     }
 }
