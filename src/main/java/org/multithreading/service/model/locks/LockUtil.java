b106c6dffb23ac0fd7d4d2a84dc68234244aab6a
Jovakinn <max05012004@gmail.com>
2021-10-31 11:44:01 +0200

    add util service

---
+++ src/main/java/org/multithreading/service/model/locks/LockUtil.java
@@ -0,0 +1,27 @@
+package org.multithreading.service.model.locks;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Logger;
+
+public class LockUtil {
+    private static final Logger logger = Logger.getLogger(LockUtil.class.getName());
+
+    public static void stopExecutor(ExecutorService executorService) {
+        try {
+            executorService.shutdown();
+            executorService.awaitTermination(10, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            logger.info("Termination interrupted");
+        } finally {
+            if (!executorService.isTerminated()){
+                logger.warning("killing non-finished task");
+            }
+            executorService.shutdownNow();
+        }
+    }
+
+    public static void sleep(Integer seconds) throws InterruptedException {
+        TimeUnit.SECONDS.sleep(seconds);
+    }
+}
