edbb796871d2b70fdd7526b9dc91bef5dfbe6b5e
Jovakinn <max05012004@gmail.com>
2021-11-02 23:10:54 +0200

    add

--- src/main/java/org/multithreading/service/model/Atomics/AtomicIntegerEx.java
+++ src/main/java/org/multithreading/service/model/Atomics/AtomicIntegerEx.java
@@ -1,4 +1,7 @@
 package org.multithreading.service.model.Atomics;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class AtomicIntegerEx {
@@ -9,12 +12,16 @@ public class AtomicIntegerEx {
     }
 
     public static void main(String[] args) throws InterruptedException {
+        List<Thread> list = Collections.synchronizedList(new ArrayList<>());
         Thread thread1 = new Thread(new MyRunnableImpl());
         Thread thread2 = new Thread(new MyRunnableImpl());
+        list.add(thread1);
+        list.add(thread2);
         thread1.start();
         thread2.start();
         thread1.join();
         thread2.join();
+        list.stream().findFirst().toString().equals("thread");
 
         System.out.println(counter);
     }
